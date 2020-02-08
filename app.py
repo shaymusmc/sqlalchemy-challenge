1ea23f102369b5e104fbfadeca3f5350a85a8582
Shaymus McTeague <Shaymus.McTeague@arm.com>
2020-02-07 21:51:57 -0800

    update to flask

--- app.py
+++ app.py
@@ -27,29 +27,12 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-#create session
-session = Session(engine)
-
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-# This function called `calc_temps` will accept start date and end date in the format '%Y-%m-%d' 
-# and return the minimum, average, and maximum temperatures for that range of dates
-def calc_temps(start_date, end_date):
-    """TMIN, TAVG, and TMAX for a list of dates.
-    
-    Args:
-        start_date (string): A date string in the format %Y-%m-%d
-        end_date (string): A date string in the format %Y-%m-%d
-        
-    Returns:
-        TMIN, TAVE, and TMAX
-    """
-    
-    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+
 #################################################
 # Flask Routes
 #################################################
@@ -71,6 +54,7 @@ def welcome():
 def precipitation():
 
     """Convert the query results to a Dictionary using date as the key and prcp as the value"""
+    session = Session(engine)
     # Design a query to retrieve the last 12 months of precipitation data and plot the results
     # Calculate the date 1 year ago from the last data point in the database
     max_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
@@ -92,19 +76,25 @@ def precipitation():
 
     return jsonify(precip_dict)
 
+    session.close()
+
 @app.route("/api/v1.0/stations")
 def stations():
 
     """Return a JSON list of stations from the dataset."""
+    session = Session(engine)
     # List of the stations and the counts in descending order.
     stations = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).\
             order_by(func.count(Measurement.station).desc()).all()
 
     return jsonify(stations)
 
+    session.close()
+
 @app.route("/api/v1.0/tobs")
 def tobs():
 
+    session = Session(engine)
     # Design a query to retrieve the last 12 months of precipitation data and plot the results
     # Calculate the date 1 year ago from the last data point in the database
     max_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
@@ -114,26 +104,28 @@ def tobs():
     # Subtract one year for start date.
     first_date = ((max_date - relativedelta(years = 1)).strftime('%Y-%m-%d'))
 
-
     # Query the last 12 months of temperature observation data for the past year.
     temp_data = session.query(Measurement.date, Measurement.tobs).\
         filter(Measurement.date >= first_date).all()
 
     return jsonify(temp_data)
 
+    session.close()
+
 @app.route("/api/v1.0/<start>")
 def start(start):
     """Return a JSON list of the minimum, average, and maximum temperatures from the start date until
     the end of the database."""
-    ## USING calc_temps AND SOME IDEATION OF RETURN LIST INSPIRED BY GITHUB USER ejhagee, THEN MODIFIED FOR MY CODE AND STYLE
+    session = Session(engine)
 
     #First we find the last date in the database
     max_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
     max_date_2 = max_date_query[0][0]
     max_date = datetime.datetime.strptime(max_date_2, "%Y-%m-%d")
 
-    # Pull min, avg, max temps utilizing the calc-temps function from the jupyter notebook
-    temps = calc_temps(start, max_date)
+    # Pull min, avg, max temps utilizing the idea of the calc-temps function from the jupyter notebook
+    temps = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= max_date).all()
 
     # Create list and dictionary to produce output.
     return_list = []
@@ -145,14 +137,16 @@ def start(start):
 
     return jsonify(return_list)
 
+    session.close()
+
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
-    """Return a JSON list of the minimum, average, and maximum temperatures from the start date unitl
-    the end date."""
-    ## USING calc_temps AND SOME IDEATION OF RETURN LIST INSPIRED BY GITHUB USER ejhagee, THEN MODIFIED FOR MY CODE AND STYLE
+    """Return a JSON list of the minimum, average, and maximum temperatures from the start date until the end date."""
+    session = Session(engine)
 
-    # Pull min, avg, max temps utilizing the calc-temps function from the jupyter notebook
-    temps = calc_temps(start, end)
+    # Pull min, avg, max temps utilizing the idea of the calc-temps function from the jupyter notebook
+    temps = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
 
     # Create list and dictionary to produce output.
     return_list = []
@@ -164,6 +158,7 @@ def start_end(start, end):
 
     return jsonify(return_list)
 
+    session.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
